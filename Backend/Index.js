1edb292a0d8f4c80963f6dcfa9ab32e825104b13
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-14 12:33:53 +0100

    Cooking...

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8888
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Facts = require("./Data/cat facts.json")
+
+const randomFacts = Facts[Math.floor(Math.random() * Facts.length)]
+
+app.get('/', (req, res) => res.json('Hey There, Welcome to Soft Random Cat Fact!'))
+app.get('/Fact', (req, res) => { res.json(randomFacts) })
+app.get('/AllFacts', (req, res) => { res.json(Facts) })
 app.listen(port, () => console.log(`Server running on port ${port}!`))
